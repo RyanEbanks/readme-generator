93846734cc4e8f01d8e27b397850587a35153a83
RyanEbanks <ryankebanks@gmail.com>
2023-02-28 12:59:36 -0500

    Added some side notes and changed how some of the functions worked, yet to test

--- index.js
+++ index.js
@@ -4,73 +4,131 @@ const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = ["Enter your project title: ", "Enter your project description: ", "Add a table of contents: ", 
 "What are the installation steps? (Y/N): ", "What is the usage for this project: ", "Would you like to add screenshots? (Y/N): ", "Add a license: ",
-"Add a license badge: ", "Would you like others to contribute? (Y/N): ", "Write tests for your application: ", "Give contact information for people with questions: "];
+"Would you like others to contribute? (Y/N): ", "Write tests for your application: ", "Give contact information for people with questions: "];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+   
+    fs.writeFile("README.md", fileName, (error, data) => {
+        if(error) {
+            console.log(error);
+        }
+        console.log(data);
+    });
+}
+
+// TODO: Create a function to initialize app
+function init() {
     inquirer.prompt([{
         type: "input",
-        name: "Project Title",
+        name: "title",
         message: questions[0]
     },
     {
         type: "input",
-        name: "Project Description",
+        name: "description",
         message: questions[1]
     },
     {
-        type: "input",
-        name: "Project Table of Contents",
-        message: questions[2]
+        //Checkbox allows me to include what I want included under table of conents
+        type: "checkbox",
+        name: "tableOfContents",
+        message: questions[2],
+        choices: ["Installation", "Usage", "Credits", "License"]
     },
+    //not sure how to handle installation yet might do a list for step by step or checkbox, need an N/A option (also might change the question phrase for it in const)
     {
         type: "input",
-        name: "Project Installation",
+        name: "projInstallation",
         message: questions[3]
     },
     {
         type: "input",
-        name: "Project Usage",
+        name: "projUsage",
         message: questions[4]
     },
+    //rawlist allows me to select 1 for yes and 2 for no
     {
-        type: "input",
-        name: "Project Screenshots",
-        message: questions[5]
+        type: "rawlist",
+        name: "screenshots",
+        message: questions[5],
+        choices: ["yes", "no"]
     },
+    //Checkbox to get license string or list from a list of options
     {
-        type: "input",
-        name: "Project License",
-        message: questions[6]
+        type: "rawlist",
+        name: "license",
+        message: questions[6],
+        //need to input choices
+        choices: []
     },
+    //Prob leave as input and write N/A if not applicable
     {
         type: "input",
-        name: "Project License Badge",
+        name: "projContribution",
         message: questions[7]
     },
+    //1 for yes and 2 for no, use JS to allow the user to type in however many steps if yes and N/A if no. Might do this or switch it back to input
     {
-        type: "input",
-        name: "Project Contribution",
+        type: "rawlist",
+        name: "projTests",
         message: questions[8]
     },
+    //provide contact questions such as email could just provide auto or have an option to provide contact or not with with JS after inputing contact (could run a name, message, default)
     {
         type: "input",
-        name: "Project Tests",
+        name: "questions",
         message: questions[9]
     },
-    {
-        type: "input",
-        name: "Project Questions",
-        message: questions[10]
-    },
 ]).then((responses) => {
-    `
-    `
-})
-}
+    //Might figure out a way to do a prompt for table of contents where it just adds these to it
+    /*
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+    */
 
-// TODO: Create a function to initialize app
-function init() {}
+//need to add javascript for license badge, based on what was selected it generates the license badge
+
+   var myMarkdown = `
+    #${responses.title}
+
+    ## Badges
+    ${licenseBadge}
+
+    ## Description
+    ${responses.description}
+
+    ## Table of Contents
+    ${responses.tableOfContents}
+
+    ## Installation
+    - [Installation](#${responses.projInstallation})
+    - [Usage](#${responses.projInstallation})
+    - [Credits](#${responses.projInstallation})
+    - [License](#${responses.projInstallation})
+
+    ## Usage
+    ${responses.projUsage}
+    
+    ${responses.screenshots}
+
+    ## License
+    ${responses.license}
+
+    ## How to Contribute
+    ${responses.projContribution}
+
+    ## Tests
+    ${responses.projTests}
+
+    ## Questions
+    ${responses.questions}
+    `
+    writeToFile(myMarkdown, responses);
+});
+}
 
 // Function call to initialize app
 init();
